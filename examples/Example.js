425e4713d7fac080abd7b768505fe1988e115c19
Michael Salaverry <michaels@wix.com>
2017-10-27 00:34:45 +0300

    adjusted example

--- examples/Example.js
+++ examples/Example.js
@@ -6,7 +6,7 @@ class Example extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sentence: [{en: 'Hello', iw: 'שלום'},{en: 'World!', iw: '!אולם'},{en: 'My name', iw: 'שם שלי'},{en: 'is', iw: 'הוא'},{en: 'Michael.', iw: '.מיכאל'}]
+            sentence: [{en: 'Hello', iw: 'שלום'},{en: 'World!', iw: 'עולם!'},{en: 'My name', iw: 'שם שלי'},{en: 'is', iw: 'הוא'},{en: 'Michael.', iw: 'מיכאל.'}]
         }
     }
 
@@ -14,6 +14,15 @@ class Example extends React.Component {
         return (
             <div>
                 <InterlinearText sentence={this.state.sentence} />
+
+                <p>
+                    English sentence: {this.state.sentence.map(word=>word.en).join(' ')}
+                </p>
+
+                <p>
+                    Hebrew sentence: 
+                    {` ${this.state.sentence.map(word=>word.iw).join(' ')}`}
+                </p>
             </div>
         )
     }
